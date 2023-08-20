3d1d2a2160e055dd48a17242e174ca75ca672049
Patrick Oliveira <patrick.oliveira@aluno.ufabc.edu.br>
2023-08-20 03:03:09 -0300

    att test

--- pythonapi/api_test.py
+++ pythonapi/api_test.py
@@ -4,18 +4,26 @@ import json
 if __name__ == "__main__":
     with open("input.isi", "r") as f:
         input_code = f.read()
+    
+    output_extension = {
+        "Java": "java",
+        "Python": "py",
+        "C": "c"
+    }
+    
+    for target_language in ["Java", "Python", "C"]:
+        response = requests.post(
+            "http://127.0.0.1:5000/compile/{}".format(
+                target_language
+            ),
+            json = {
+                "code": input_code
+            }
+        )
         
-    response = requests.post(
-        "http://127.0.0.1:5000/compile/{}".format(
-            "Java"
-        ),
-        json = {
-            "code": input_code
-        }
-    )
-    
-    assert response.status_code == 200, "Error"
-    
-    output_code = json.loads(response.content)["output_code"]
-    
-    print(output_code)
+        assert response.status_code == 200, "Error"
+        
+        output_code = json.loads(response.content)["output_code"]
+        
+        with open("target_code.{}".format(output_extension[target_language]), "w") as f:
+            f.write(output_code)
